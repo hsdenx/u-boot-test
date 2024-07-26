ba60d987e6aa08b536572420277680af58bf0e68
Heiko Schocher <hs@denx.de>
2024-07-26 07:03:06 +0200

    wdt: imx8qxp: add option to control external PMIC wdt via IMX8 SCU
    control the watchdog in the PMIC through SCU.
    
    Signed-off-by: Andrej Valek <andrej.valek@siemens.com>
    Signed-off-by: Heiko Schocher <hs@denx.de>
    Series-version: 1
    Series-to: u-boot
    Series-cc: Walter Schweizer <walter.schweizer@siemens.com>
    Series-cc: Alexander Sverdlin <alexander.sverdlin@siemens.com>
    Series-cc: Enrico Leto <enrico.leto@siemens.com>
    
    Cover-letter:
    imx8qxp: siemens board: updates / sync with mainline
    Updating the imx8qxp based siemens board. The current
    state of the siemens imx8qxp board is in an old not
    really working state.
    
    This series brings mainline back working on the siemens
    deneb board, the guedi board is dropped as not longer used.
    
    make all runs fine without having all the needed firmware images
    on board, and so CI @azure.
    
    If you want to have a working image you need to copy the
    following images to your build directory $BDIR:
    
    ATF : bl31-imx8dx.bin            -> $BDIR/bl31.bin
    AHAB: mx8qxc0-ahab-container.img -> $BDIR/mx8qxc0-ahab-container.img
    SCFW: deneb-scfw-tcm.bin         -> $BDIR/mx8qx-mek-scfw-tcm.bin
    
    and call make flash.bin
    
    There are also updates for ethernet and USB support, but not
    ready to push upstream currently, hopefully following soon.
    
    Azure build:
    https://dev.azure.com/hs0298/hs/_build/results?buildId=131&view=results
    
    patchseries needs patch:
    https://patchwork.ozlabs.org/project/uboot/patch/20241104180203.110395-1-hs@denx.de/
    
    series is based on mainline commit:
    56accc56b9a ("bios_emulator: fix first argument of pci_{read,write}_config_* function calls")
    
    END

---
+++ drivers/watchdog/scu_wdt.c
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <dm.h>
+#include <wdt.h>
+#include <firmware/imx/sci/sci.h>
+
+/* watchdog commands */
+#define CMD_START_WDT 0x55
+#define CMD_STOP_WDT  0x45
+#define CMD_PING_WDT  0x35
+
+static int scu_wdt_start(struct udevice *dev, u64 timeout_ms, ulong flags)
+{
+	/* start external watchdog via Timer API */
+	return sc_timer_control_pmic_wdog(-1, CMD_START_WDT);
+}
+
+static int scu_wdt_stop(struct udevice *dev)
+{
+	/* stop external watchdog via Timer API */
+	return sc_timer_control_pmic_wdog(-1, CMD_STOP_WDT);
+}
+
+static int scu_wdt_reset(struct udevice *dev)
+{
+	return sc_timer_control_pmic_wdog(-1, CMD_PING_WDT);
+}
+
+static int scu_wdt_probe(struct udevice *dev)
+{
+	debug("%s(dev=%p)\n", __func__, dev);
+	return 0;
+}
+
+static const struct wdt_ops scu_wdt_ops = {
+	.reset		= scu_wdt_reset,
+	.start		= scu_wdt_start,
+	.stop		= scu_wdt_stop,
+};
+
+static const struct udevice_id scu_wdt_ids[] = {
+	{ .compatible = "siemens,scu-wdt" },
+	{ }
+};
+
+U_BOOT_DRIVER(scu_wdt) = {
+	.name		= "scu_wdt",
+	.id		= UCLASS_WDT,
+	.of_match	= scu_wdt_ids,
+	.probe		= scu_wdt_probe,
+	.ops		= &scu_wdt_ops,
+};
